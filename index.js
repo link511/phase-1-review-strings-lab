6ed28645edb3914df12578dca62e7d88fbf773da
Lincoln <lincoln@taylorlivingco.com>
2022-08-02 14:08:34 +1000

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+let currentUser = 'skywalker';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}, !`;
+
+//using the previous variable and change to uppper case
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// slice (start - 0 first character  ,    end - up to and not including)
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}, !`;
